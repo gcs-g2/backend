0b835aced5bed71d1f744cee5836b3464893c447
Manva Pradhan <pradhanmanva@gmail.com>
2017-09-17 02:16:35 -0500

    CRUD operation!!

--- Database.py
+++ Database.py
@@ -1,55 +1,65 @@
 import pymysql as PyMySQL
-import jsonpickle as JsonPickle
+
 
 def open_connection():
     db = PyMySQL.connect("localhost", "root", "", "edubot")
     return db
 
+
 def close_connection(db):
     var = db.close
-    print(var)
+    print("connection closed")
 
-def getID(table_name):
-    db = open_connection()
-    cursor = db.cursor()
-    num=0
+
+def getID():
+    num = 0
     sql = "SELECT max(id) FROM noteshistory"
+    id = 0
     try:
         cursor.execute(sql)
-        num=cursor.fetchone()+1
+        num = cursor.fetchone()
+        id = num[0] + 1
         db.commit()
     except:
         db.rollback()
-    close_connection(db)
-    return num
+    return id
 
-def insert_into_note_list():
-    db = open_connection()
-    cursor = db.cursor()
-    id = getID("noteshistory")
-    sql = "INSERT INTO noteshistory (id,note_text,time) VALUES (" + str(id) + ",'Hey','1:1:1')"
+
+def insert_into_note_list(db, cursor):
+    id = getID()
+    sql = "INSERT INTO noteshistory (id, title, note_text, time, status) VALUES (" + str(
+        id) + ",'artificial intelligence','assignment is due','4:5:6','pending')"
     try:
         cursor.execute(sql)
         db.commit()
     except:
         db.rollback()
-    close_connection(db)
 
-def select_all_notes():
-    db = open_connection()
-    cursor = db.cursor()
-    sql = "select * from noteshistory"
+
+def select_all_notes(db, cursor):
+    sql = "SELECT * FROM noteshistory"
     try:
         cursor.execute(sql)
         result = cursor.fetchall()
         print(result)
-        print("\n")
-        print(JsonPickle.encode(result))
         db.commit()
     except:
         db.rollback()
-    close_connection(db)
+
+
+def update_record(id, new_text):
+    sql = "UPDATE noteshistory SET note_text='" + new_text + "' WHERE id=" + str(id)
+    try:
+        cursor.execute(sql)
+        db.commit()
+    except:
+        db.rollback()
+
 
 if __name__ == "__main__":
-    insert_into_note_list()
-    select_all_notes()
+    db = open_connection()
+    cursor = db.cursor()
+    # insert_into_note_list(db, cursor)
+    # select_all_notes(db, cursor)
+    update_record(2, "Jaanu is cute")
+    close_connection(db)
