4d9422d3e49eebead1edc56efe0324b23e55e6df
Ishan <contact@ishansharma.com>
2017-09-16 21:49:17 -0500

    Adding support for notes endpoints

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
 from flask import Flask
 from flask import jsonify
 from flask import request
+import json
+
 from chat import *
+from notes import *
 
 app = Flask(__name__)
 
@@ -19,6 +22,23 @@ def handle_chat():
 
     if request.method == 'PUT':
         return jsonify(save_chat())
+
+@app.route('/notes')
+def return_all_notes():
+    return jsonify(get_all_notes())
+
+@app.route('/note/<note_id>')
+def get_single_note(note_id):
+    return jsonify(get_note(note_id))
+
+@app.route('/note', methods=['POST', 'PUT'])
+def handle_note_update_or_create():
+    data = json.loads(request.data)
+
+    if data['id']:
+        return jsonify(update_note(data))
+    else:
+        return jsonify(create_note(data))
 
 
 if __name__ == "__main__":
---
+++ notes.py
@@ -0,0 +1,42 @@
+def get_note(note_id):
+    return {
+        "id": note_id,
+        "title": 'Artificial Intelligence',
+        "text": 'Artificial Intelligence is very very important',
+        "date": '',
+        "status": 'pending'
+    }
+
+
+def get_all_notes():
+    return [
+        {
+            "id": 1,
+            "title": 'Artificial Intelligence',
+            "text": 'Artificial Intelligence is very very important',
+            "date": '',
+            "status": 'pending'
+        },
+        {
+            "id": 2,
+            "title": 'DAA',
+            "text": 'DAA is the most important',
+            "date": '',
+            "status": 'pending'
+        },
+        {
+            "id": 3,
+            "title": 'DBMS',
+            "text": 'DBMS can be flunked!',
+            "date": '',
+            "status": 'pending'
+        }
+    ]
+
+def update_note(data):
+    print('inside data')
+    print(data)
+    return data
+
+def create_note(data):
+    return data
