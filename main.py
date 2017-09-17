493e4f97ab8779ce44d2a829f92730fab55e022b
Ishan <contact@ishansharma.com>
2017-09-17 00:25:38 -0500

    Formatting + CORS Changes

--- main.py
+++ main.py
@@ -9,9 +9,20 @@ from notes import *
 
 app = Flask(__name__)
 
+
+# allow cross origin requests
+@app.after_request
+def after_request(response):
+    response.headers.add('Access-Control-Allow-Origin', '*')
+    response.headers.add('Access-Control-Allow-Headers', 'Content-Type,Authorization')
+    response.headers.add('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS')
+    return response
+
+
 @app.route('/')
 def handle_default():
     return 'Try either /chat or /note'
+
 
 @app.route('/chat', methods=['GET', 'POST', 'PUT'])
 def handle_chat():
@@ -24,13 +35,16 @@ def handle_chat():
     if request.method == 'PUT':
         return jsonpickle.encode(save_chat())
 
+
 @app.route('/notes')
 def return_all_notes():
     return jsonpickle.encode(get_all_notes())
 
+
 @app.route('/note/<note_id>')
 def get_single_note(note_id):
     return jsonpickle.encode(get_note(note_id))
+
 
 @app.route('/note', methods=['POST', 'PUT'])
 def handle_note_update_or_create():
@@ -44,4 +58,3 @@ def handle_note_update_or_create():
 
 if __name__ == "__main__":
     app.run()
-
